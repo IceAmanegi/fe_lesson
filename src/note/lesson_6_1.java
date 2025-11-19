0a53a9747f7a1146d73535c1c85167860fcdb26f
IceAmanegi <iceamanegi@gmail.com>
2025-11-19 22:02:58 +0900

    レッスン６の１

---
+++ src/note/lesson_6_1.java
@@ -0,0 +1,29 @@
+//セクション６：繰り返し
+package note;
+
+public class lesson_6_1 {
+    public static void main(String[] args) {
+//        for(int num = 1; num <= 10; num += 1){
+//            System.out.println(num);
+//        }
+//        for(int num1 = 10; num1 >= 1; num1 = num1 - 1){
+//            System.out.println(num1);
+//        }
+
+        //入れ替え
+        int [] array = new int []{1, 2, 3, 4, 5};
+        int right;
+        int tmp;
+
+        for(int left = 0 ; left <= (array.length - 1) / 2 ; left += 1) {
+            right = (array.length - 1) - left;
+            tmp = array[right];
+            array[right] = array[left];
+            array[left] = tmp;
+        }
+        for(int i = 0 ; i <= array.length - 1 ; i ++ ) {
+            System.out.println(array[i]);
+        }
+
+    }
+}
